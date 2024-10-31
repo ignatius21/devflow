81d1c6586ca1f0b7b44bb5a111772452c47a9ed8
ignatius21 <ignaciojosealberto@hotmail.com>
2024-10-31 20:05:53 -0300

    Actualiza estilos de la página principal y agrega nuevas dependencias de Tailwind CSS para mejorar la tipografía y animaciones

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-    <h1 className=" text-3xl font-bold text-purple-500">Welcome to Next Js</h1>
+    <h1 className="h1-bold text-primary-500">Welcome to Next Js</h1>
   );
 }
